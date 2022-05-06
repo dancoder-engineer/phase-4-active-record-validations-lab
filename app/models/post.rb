41c009ceb7046d01166cb25954238ce06597db46
Dan Seminara <japaneseforgaming@gmail.com>
2022-05-06 09:49:56 -0400

    commit tree 0e968ddb65f6194bd5ea9542af430170f878b8f1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+ #   validate :clickbait2
+
+    def clickbait
+        if title == "True Facts"
+            errors.add(:title, "Too clickbaity")
+        end
+    end
+
+ #   def clickbait2
+ #       if title.match?(/flatironschool.com/)
+ #           errors.add(:title, "We're only allowed to have people who work for the company in the database!")       
+ #       end
+ #   end
 end
